2dd432f8e58189bd7dd262a777b98420a859306c
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-04-16 21:02:10 +0200

    changes

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,22 +6,31 @@ const sources = [
   {
     "name": "Hidden Figures",
     "sourceType": "FILMS",
-    "image": "https://pics.filmaffinity.com/hidden_figures-810983135-large.jpg",
-    "url": "https://pics.filmaffinity.com/hidden_figures-810983135-large.jpg"
+    "image": "http://shaw.sg/Images/Film/DH473/m-hiddenfigures-v2.jpg",
+    "url": "http://shaw.sg/Images/Film/DH473/m-hiddenfigures-v2.jpg"
   },
   {
     "name": "Margaret Hamilton",
     "sourceType": "FIGURES",
     "image": "https://pbs.twimg.com/media/CLfShJ5UwAAFbqi.jpg",
-    "url": "https://pics.filmaffinity.com/hidden_figures-810983135-large.jpg"
+    "url": "http://shaw.sg/Images/Film/DH473/m-hiddenfigures-v2.jpg"
 
-  },{
-      "name": "Girls who code",
-      "sourceType": "BOOKS",
-      "image": "https://3zjc852t4swp1lmezl171oga-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/GWC_finalcover.jpg",
-      "url": "https://pics.filmaffinity.com/hidden_figures-810983135-large.jpg"
+  },
+  {
+    "name": "Carlos Prado",
+    "sourceType": "MENTORS",
+    "image": "https://avatars2.githubusercontent.com/u/11295347?s=460&v=4",
+    "url": "http://shaw.sg/Images/Film/DH473/m-hiddenfigures-v2.jpg"
+
+  },
+  {
+    "name": "Girls who code",
+    "sourceType": "BOOKS",
+    "image": "https://3zjc852t4swp1lmezl171oga-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/GWC_finalcover.jpg",
+    "url": "http://shaw.sg/Images/Film/DH473/m-hiddenfigures-v2.jpg"
 
   }
+
 ];
 
 Source.create(sources)
